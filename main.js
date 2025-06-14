566e64af61725d3e98dfe0b303a1920596041132
Richard Fontoura <richafo.da2015@gmail.com>
2025-06-14 09:26:44 -0300

    feat(hub): implement v2 of about form with Foundry V13 support
    - Refactor AboutForm to AboutFormV2 using ApplicationV2 API
    - Update module.json for v2.0.0 compatibility
    - Improve tool registration system with Map structure
    - Add responsive height adjustment based on module count
    - Simplify Patreon link text in template
    - Update CSS for better maintainability and performance

--- main.js
+++ main.js
@@ -1,65 +1,145 @@
-import { AboutForm } from "./src/about-form.js";
+import { AboutFormV2 } from "./src/about-formV2.js";
 
 class LoboWerewolfHub {
-    static registeredTools = [];
+  static registeredTools = new Map();
 
-    static init() {
-        console.log(game.i18n.localize("Werewolf.Initializing"));
+  static init() {
+    console.log(game.i18n.localize("Werewolf.Initializing"));
 
-        Handlebars.registerHelper('lte', function(a, b) {
-            return a <= b;
-        });
+    Handlebars.registerHelper("lte", (a, b) => a <= b);
 
-        Hooks.on('getSceneControlButtons', this._getSceneControlButtons.bind(this));
-        game.lobowerewolfHub = {
-            registerTool: this.registerTool.bind(this)
-        };
+    this.registerTool({
+      name: "defaultTool",
+      title: "Welcome to the LoboWerewolf Hub!",
+      icon: "fas fa-home",
+      button: false,
+      visible: false,
+      order: 0,
+      onClick: () => {},
+    });
+
+    const formOptions = {
+      name: "aboutButton",
+      title: "About",
+      icon: "fa-solid fa-info",
+      button: true,
+      visible: true,
+      order: 1,
+    };
+
+    this.registerTool({
+      ...formOptions,
+      onClick: () => {
+        new AboutFormV2().render(true);
+      },
+    });
+
+    Hooks.on("getSceneControlButtons", this._getSceneControlButtons.bind(this));
+  }
+
+  static registerTool({
+    name,
+    title,
+    icon,
+    button,
+    toggle = false,
+    visible,
+    order,
+    onClick,
+  }) {
+    if (!name || !title || !icon || typeof onClick !== "function") {
+      console.error(game.i18n.localize("Werewolf.ToolRegisterError"), {
+        name,
+        title,
+        icon,
+      });
+      return false;
     }
 
-    /**
-     * @param {Array} controls 
-     */
-    static _getSceneControlButtons(controls) {
-        controls.push({
-            name: "lobowerewolfhub",
-            title: game.i18n.localize("Werewolf.HubTitle"),
-            icon: "fa-brands fa-wolf-pack-battalion",
-            layer: "controls",
-            tools: this.registeredTools
-        });
+    let finalOrder = order;
+    if (!order) {
+      const toolCount = this.registeredTools.size;
+      finalOrder = toolCount + 1;
     }
 
-    /**
-     * @param {Object} toolData 
-     * @returns {Boolean} 
-     */
-    static registerTool(toolData) {
-        if (!toolData || !toolData.name || !toolData.title || !toolData.icon) {
-            console.error(game.i18n.localize("Werewolf.ToolRegisterError"), toolData);
-            return false;
-        }
+    this.registeredTools.set(name, {
+      name,
+      title,
+      icon,
+      button,
+      toggle,
+      visible,
+      order: finalOrder,
+      onClick,
+    });
 
-        this.registeredTools.push(toolData);
-        console.log(game.i18n.format("Werewolf.ToolRegistered", { name: toolData.name }));
-        return true;
+    console.log(game.i18n.format("Werewolf.ToolRegistered", { name }));
+
+    if (ui.controls) {
+      const controls = ui.controls.controls;
+      this.setV13Controls(controls, Object.keys(controls).length + 1);
     }
+
+    return true;
+  }
+
+  static setV13Controls(controls, order) {
+    const toolMap = {};
+    for (const [key, tool] of this.registeredTools.entries()) {
+      toolMap[key] = {
+        name: key,
+        title: tool.title,
+        icon: tool.icon,
+        button: tool.button,
+        toggle: tool.toggle,
+        visible: tool.visible,
+        order: tool.order,
+        onClick: tool.onClick,
+        onChange: () => {},
+      };
+    }
+
+    const firstTool = Object.keys(toolMap)[0] || null;
+
+    controls.lobowerewolfhub = {
+      name: "lobowerewolfhub",
+      title: game.i18n.localize("Werewolf.HubTitle"),
+      icon: "fas fa-paw",
+      layer: "lobowerewolfhub",
+      order,
+      visible: true,
+      activeTool: firstTool,
+      tools: toolMap,
+      onChange: () => {},
+      onClick: () => {},
+    };
+  }
+
+  /**
+   * @param {SceneControl[] | Record<string, SceneControl>} controls
+   */
+  static _getSceneControlButtons(controls) {
+    const coreVersion = game.version;
+    const isPre13 = foundry.utils.isNewerVersion("13", coreVersion);
+    const order = isPre13
+      ? controls.length + 1
+      : Object.keys(controls).length + 1;
+
+    if (isPre13) {
+      controls.push({
+        name: "lobowerewolfhub",
+        title: game.i18n.localize("Werewolf.HubTitle"),
+        icon: "fa-brands fa-wolf-pack-battalion",
+        layer: "controls",
+        tools: Array.from(this.registeredTools.values()),
+      });
+    } else {
+      this.setV13Controls(controls, order);
+    }
+  }
 }
 
-Hooks.once('init', () => {
-    LoboWerewolfHub.init();
-});
-
-Hooks.on("ready", () => {
-    if (!game.lobowerewolfHub) return;
-    game.lobowerewolfHub.registerTool({
-        name: "aboutButton",
-        title: "About",
-        icon: "fa-solid fa-info",
-        button: true,
-        visible: true,
-        onClick: () => {
-            const form = new AboutForm();
-            form.render(true);
-        }
-    })
+Hooks.once("init", () => {
+  game.lobowerewolfHub = LoboWerewolfHub;
+  LoboWerewolfHub.init();
 });
--- src/about-form.js
+++ src/about-form.js
@@ -1,72 +1,74 @@
 export class AboutForm extends FormApplication {
-    constructor(object = {}, options = {}) {
-        super(object, options);
-        this.modules = [];
-        
-        const cssPath = "modules/lobowerewolf-hub/styles/about-form.css";
-        if (!document.querySelector(`link[href="${cssPath}"]`)) {
-            const link = document.createElement('link');
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            link.href = cssPath;
-            document.head.appendChild(link);
-        }
+  constructor(object = {}, options = {}) {
+    super(object, options);
+    this.modules = [];
+
+    const cssPath = "modules/lobowerewolf-hub/styles/about-form.css";
+    if (!document.querySelector(`link[href="${cssPath}"]`)) {
+      const link = document.createElement("link");
+      link.rel = "stylesheet";
+      link.type = "text/css";
+      link.href = cssPath;
+      document.head.appendChild(link);
+    }
+  }
+
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      id: "about-form",
+      title: "About LoboWerewolf",
+      template: "modules/lobowerewolf-hub/templates/about-form.html",
+      width: 700,
+      height: 695,
+      closeOnSubmit: true,
+      resizable: false,
+      classes: ["lobowerewolf-about"],
+    });
+  }
+
+  async getData(options) {
+    const data = super.getData(options);
+    try {
+      const response = await fetch(
+        "https://lobowerewolf.squareweb.app/api/modules"
+      );
+      if (response.ok) {
+        const text = await response.text();
+        data.modules = JSON.parse(text);
+      } else {
+        console.error("Failed to load modules list:", response.status);
+        data.modules = [];
+      }
+    } catch (error) {
+      console.error("Error loading modules list:", error);
+      data.modules = [];
+    }
+    return data;
+  }
+
+  async _renderInner(...args) {
+    const html = await super._renderInner(...args);
+    if (this.moduleCount !== undefined) {
+      let height;
+
+      if (this.moduleCount <= 3) {
+        height = 600;
+      } else if (this.moduleCount <= 6) {
+        height = 700;
+      } else {
+        height = 800;
+      }
+      this.options.height = height;
+
+      if (this.element && this.element.length) {
+        this.setPosition({ height });
+      }
     }
 
-    static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            id: "about-form",
-            title: "About LoboWerewolf",
-            template: "modules/lobowerewolf-hub/templates/about-form.html",
-            width: 700,
-            height: 695, 
-            closeOnSubmit: true,
-            resizable: false,
-            classes: ["lobowerewolf-about"]
-        });
-    }
+    return html;
+  }
 
-    async getData(options) {
-        const data = super.getData(options);     
-        try {
-            const response = await fetch("https://lobowerewolf.squareweb.app/api/modules");
-            if (response.ok) {
-                const text = await response.text();
-                data.modules = JSON.parse(text);
-            } else {
-                console.error("Failed to load modules list:", response.status);
-                data.modules = [];
-            }
-        } catch (error) {
-            console.error("Error loading modules list:", error);
-            data.modules = [];
-        }  
-        return data;
-    }
-
-    async _renderInner(...args) {
-        const html = await super._renderInner(...args);
-        if (this.moduleCount !== undefined) {
-            let height;
-            
-            if (this.moduleCount <= 3) {
-                height = 600; 
-            } else if (this.moduleCount <= 6) {
-                height = 700; 
-            } else {
-                height = 800; 
-            }
-            this.options.height = height;
-        
-            if (this.element && this.element.length) {
-                this.setPosition({height});
-            }
-        }
-        
-        return html;
-    }
-
-    async _updateObject(event, formData) {
-        // 
-    }
+  async _updateObject(event, formData) {
+    //
+  }
 }
